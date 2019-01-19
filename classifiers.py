af0b50e2b9fb45379ac0d9db417da6f77df6ccf1
coryswainston <swainston.cory89@gmail.com>
2019-01-19 16:45:56 -0700

    implement k nearest neighbors

--- classifiers.py
+++ classifiers.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Just kind of a stub for a classifier
+File for different classifiers
 
 @author: coryswainston
 """
+import numpy as np
 
 class HardCodedClassifier:
     def __init__(self):
@@ -16,3 +17,32 @@ class HardCodedClassifier:
     def predict(self, data):
         # we're bound to be right sometimes?
         return [0 for i in data]
+    
+class KNearestNeighbors:
+    def __init__(self, k):
+        self.k = k
+        
+    def fit(self, data, targets):
+        self.data = data
+        self.targets = targets
+        
+    def predict(self, data):
+        assert(np.shape(data)[1] == np.shape(self.data)[1])
+        targets = []                
+        for row in data:
+            targets.append(self._get_target_for_row(row))                
+        return targets
+    
+    def _get_target_for_row(self, row):
+        # get the distance of each datapoint along with its target
+        distances_by_target = []
+        for srow, starget in zip(self.data, self.targets):
+            distances = [(x-y)**2 for x, y in zip(srow, row)]
+            distances_by_target.append((starget, sum(distances)))
+        # sort from smallest to largest
+        distances_by_target.sort(key=lambda tup: tup[1])
+        # get k nearest neighbors
+        nearest_neighbors = [i[0] for i in distances_by_target[0:self.k]]
+        # find the most common classification
+        return max(set(nearest_neighbors), key=nearest_neighbors.count) 
+        
---
+++ knn.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Prediction using k nearest neighbors algorithm
+
+@author: coryswainston
+"""
+
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from classifiers import KNearestNeighbors
+
+# get the iris data
+iris = datasets.load_iris()
+X = iris.data
+y = iris.target
+
+# split and randomize the data
+test_size = .30
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
+
+# get k value
+k = int(input("Enter number of neighbors: "))
+while k < 1 or k > 20:
+    k = int(input("Enter a value between 1 and 20: "))
+
+# first run it through a real classifier
+classifier = KNeighborsClassifier(n_neighbors=k)
+classifier.fit(X_train, y_train)
+prediction = classifier.predict(X_test)
+results = prediction == y_test
+print("\nsklearn Results:")
+print(results)
+
+# this one is just a hard-coded result every time
+my_classifier = KNearestNeighbors(k)
+my_classifier.fit(X_train, y_train)
+my_prediction = my_classifier.predict(X_test)
+my_results = my_prediction == y_test
+print("\nMy results:")
+print(my_results)
+
+# compare accuracy
+print("\nAccuracy:")
+
+correct_set = len([i for i in results if i])
+accuracy = correct_set / len(results)
+print(f"sklearn KNeighbors: {accuracy:.2f}")
+
+my_correct_set = len([i for i in my_results if i])
+my_accuracy = my_correct_set / len(my_results)
+print(f"My algorithm: {my_accuracy:.2f}")
