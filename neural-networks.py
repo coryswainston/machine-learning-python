ccb8281381a2fe4ed4f22366fb1c03bef8d9ae8d
Cory Swainston <swainston.cory89@gmail.com>
2019-02-23 14:16:49 -0700

    neural networks

--- neural-networks.py
+++ neural-networks.py
@@ -1,17 +1,19 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Using neural networks
+Using neural networks with sklearn
 
 @author: coryswainston
 """
 
 import numpy as np
 import pandas as pd
-#import tensorflow as tf
+from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report,confusion_matrix
 from sklearn import preprocessing
 
+#################### DATA PREPARATION ####################
 
 # load at least one dataset
 cols = ["class", "handicapped-infants", "water-project", 
@@ -21,20 +23,31 @@ cols = ["class", "handicapped-infants", "water-project",
 data = pd.read_csv('data/voting.data.txt', 
                    index_col=False, 
                    names=cols, 
-                   skipinitialspace=True,
-                   na_values="?")
+                   skipinitialspace=True)
 og_data = data.copy()
 
 # prepare the dataset for a neural network
 #   handle missing data, normalize numeric values, etc.
-data.replace('y', 'c')
-data.replace('n', 0)
+data.replace('y', 1, inplace=True)
+data.replace('n', 0, inplace=True)
+data.replace('?', 1, inplace=True) #TODO fix this
+lb_enc = preprocessing.LabelEncoder()
+lb_enc.fit(['democrat', 'republican'])
+data["class"] = lb_enc.transform(data["class"])
 
 
-# train a neural network
+y = data["class"].values
+X = data.drop(columns=['class']).values
 
-# make predictions
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-# experiment with at least three sets of hyper-parameters
-#   (number of layers, nodes per layer,
-#    learning rates, activation functions, etc.)
+
+
+################# TRAINING AND PREDICTION ######################
+
+mlp = MLPClassifier(hidden_layer_sizes=(30,30,30), learning_rate_init=0.01)
+mlp.fit(X_train, y_train)
+predictions = mlp.predict(X_test)
+
+print("\n\nDATA FOR VOTING DATASET (binary data)\n")
+print(classification_report(y_test,predictions))
